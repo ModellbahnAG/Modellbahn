1508a71c7e4777624fde3f79f8fc683253f88d0c
Dennis Moschina <dennis@moschina.de>
2020-11-11 22:13:13 +0100

    added NeoPixel library

---
+++ libraries/NeoPixel/NeoPixel.h
@@ -0,0 +1,34 @@
+#ifndef MY_NEOPIXEL_H
+#define MY_NEOPIXEL_H
+
+#include <Adafruit_NeoPixel.h>
+
+enum color {
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE
+};
+
+class NeoPixel {
+  private:
+    byte pin;
+    int number;
+    Adafruit_NeoPixel pixels;
+
+  public:
+    NeoPixel(byte pin, int number);
+    void init();
+
+    void setColor(byte pixel, int r, int g, int b);
+    void setColor(byte pixel, color color);
+
+    void setColor(int r, int g, int b);
+    void setColor(color color);
+
+    void off();
+
+    byte getPin();
+};
+
+#endif
