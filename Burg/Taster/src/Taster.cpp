fb339fd98765adbb30bc70296d7369f4da550ec8
Dennis Moschina <dennis@moschina.de>
2021-01-05 17:36:28 +0100

    improved documentation for taster

--- Burg/Taster/src/Taster.cpp
+++ Burg/Taster/src/Taster.cpp
@@ -7,28 +7,31 @@
 #define NUM_BUTTON 5
 
 // (Pin des Tasters (0-7), Pin des Lichts (gerade Zahlen 34-48), (optional: Zeit, die der Taster deaktiviert ist in Sekunden))
-Button buttonArr[NUM_BUTTON] = {Button(7, 34),
-                                    Button(6, 36),
-                                    Button(5, 38),
-                                    Button(4, 40),
-                                    Button(3, 42)};   // Addressen müssen bei Master und Slave manuell eingestellt werden!!!
+Button buttonArr[NUM_BUTTON] = {Button(7, 34, 10),
+                                Button(6, 36, 10),
+                                Button(5, 38, 10),
+                                Button(4, 40, 10),
+                                Button(3, 42, 10)};   // Addressen müssen bei Master und Slave manuell eingestellt werden!!!
 byte slaveAddr[NUM_BUTTON] = {0, 1, 2, 3, 4};
 
 
 void sendStart(byte address) {
-  Wire.begin(address);
+  Wire.beginTransmission(address);
   Wire.write(1);
   Wire.endTransmission();
+  Serial.println(address);
 }
 
 
 void setup() {
   Wire.begin();
+  Wire.setClock(10000);
 
   Serial.begin(9600);
 
   for(int i = 0; i < NUM_BUTTON; i++) {
     buttonArr[i].setCallback(CallLambda([i] () {
+      Serial.println(i);
       sendStart(slaveAddr[i]);
       return 0;
     }));
