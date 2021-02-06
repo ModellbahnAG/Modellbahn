6ea0362bb2d3f2ad5c8be51965112c049d530833
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-02-06 18:52:59 +0100

    added comments

--- Burg/Grubenbahn/src/Grubenbahn.cpp
+++ Burg/Grubenbahn/src/Grubenbahn.cpp
@@ -5,9 +5,12 @@
 #define TRAIN_PIN 12
 #define LED_PIN 11
 
-boolean shouldStartTrain = false;
+// Zeit die die Lampen beim Flackern aus sind
+#define FLICKERING_TIME 5
+// Helligkeit der Beleuchtung
+#define LIGHTING_BRIGHTNESS 50
 
-int helligkeitLed = 50;
+boolean shouldStartTrain = false;
 
 void startTrain() {
   digitalWrite(TRAIN_PIN, HIGH);
@@ -15,6 +18,11 @@ void startTrain() {
   digitalWrite(TRAIN_PIN, LOW);
 }
 
+/* Setze shouldStartTrain auf true wenn eine Signal vom Taster Arduino ankommt
+*
+* Die eigentlich Explosion kann nicht in dieser Funktion ausgeführt werden, da
+* es bei Interrupt Funktionen Probleme mit delay gibt.
+*/
 void receiveEvent(int howMany) {
   shouldStartTrain = Wire.read();
 }
@@ -28,14 +36,15 @@ void setup() {
 
   randomSeed(analogRead(A0));
 
-  analogWrite(LED_PIN, helligkeitLed);
+  analogWrite(LED_PIN, LIGHTING_BRIGHTNESS);
 }
 
 void loop() {
+  // lasse die Beleuchtung zufällig flackern
   if (random(1, 10000) == 1) {
     analogWrite(LED_PIN, 0);
-    delay(5);
-    analogWrite(LED_PIN, helligkeitLed);
+    delay(FLICKERING_TIME);
+    analogWrite(LED_PIN, LIGHTING_BRIGHTNESS);
   }
 
   if (shouldStartTrain) {
